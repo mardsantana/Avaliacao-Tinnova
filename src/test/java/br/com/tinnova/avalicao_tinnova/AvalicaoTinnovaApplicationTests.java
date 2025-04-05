9834eee3572ad9f2ba26ecdcb670387364980e3b
mardsantana <mardsonsantana105@gmail.com>
2025-04-05 18:24:31 -0300

    inicia projeto

---
+++ src/test/java/br/com/tinnova/avalicao_tinnova/AvalicaoTinnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tinnova.avalicao_tinnova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvalicaoTinnovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
